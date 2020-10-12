56679aba5b9075703e9cb3c7127e26d9c5c93c30
rmwxxwmr <jadchen@qq.com>
2020-10-12 09:41:01 +0800

    3

---
+++ gateway-zuul/src/main/java/fbk/cjd/demo/cloud/gatewayzuul/filter/DemoZuulFilter.java
@@ -0,0 +1,36 @@
+package fbk.cjd.demo.cloud.gatewayzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author chenjd
+ * @date 2020/10/12 9:22
+ */
+@Component
+public class DemoZuulFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        //todo
+        return null;
+    }
+}
